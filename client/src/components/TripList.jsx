cdfb9e4357e0c6a9cec33c7a8249ed7f730c3293
Sujaree <sujaree@gmail.com>
2026-07-07 14:29:57 +0700

    feat: build homepage ui with search and trip list

---
+++ client/src/components/TripList.jsx
@@ -0,0 +1,26 @@
+import TripCard from "./TripCard";
+
+function TripList(props) {
+  const trips = props.trips;
+
+  if (trips.length === 0) {
+    return <p className="trip-list__empty">ไม่พบที่เที่ยว</p>;
+  }
+
+  return (
+    <div className="trip-list">
+      {trips.map(function (trip) {
+        return (
+          <TripCard
+            key={trip.eid}
+            trip={trip}
+            onReadMore={props.onReadMore}
+            onTagClick={props.onTagClick}
+          />
+        );
+      })}
+    </div>
+  );
+}
+
+export default TripList;
